5e797b4a518875e8ca4cb75a5df48948691715f1
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 21:35:17 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -10,6 +10,8 @@
 // implement visibility, ie only see you country and up to N border of border tiles; also make the rendering and clicking based not on the actual map, but on the tile data in the country struct (this will actually be quite painfull without adding a X and Y properties of the tile struct); also make the rendering the very last thing in the main loop again (if we want to sychronise the clicks with the map rather than the data in the country struct)
 //
 // improve rendering performance by using buffering of some sort
+//
+// delete dead countries from countries vec; bring back dead ccountry scan; remove tile checks
 
 // mouse event tracking: https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Any-event-tracking
 // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797?permalink_comment_id=3878578
