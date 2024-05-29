6ffde27f615c3ce5fb8aa0e68e30a254acf0bb07
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 16:02:18 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -2,6 +2,8 @@
 // TODO
 //
 // the fucking vectors can realloc, so I need to check what happens if I use push_back in a function that got passed a pointer/reference to the vector (nothing bad should happen, but I need to make sure)
+//
+// make it so that if the defender just sits there there is some kind of bonus, for example: if the defender has more equipment than you they get a deffensive bonus?
 
 // mouse event tracking: https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Any-event-tracking
 // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797?permalink_comment_id=3878578
