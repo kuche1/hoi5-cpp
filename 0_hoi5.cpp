802df990ab9d7c586712f3965de2c1a1f69081b6
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 13:13:40 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -745,20 +745,7 @@ int main() {
 
     int gui_additional_turns_to_pass = 0;
 
-    // main loop
-
-    for(;;){
-
-        #include "2_1_calculate_and_update_properties.cpp"
-
-        #include "2_2_draw_gui_and_get_player_input.cpp"
-
-        #include "2_3_ai_input.cpp"
-
-        #include "2_4_process_automatic_actions.cpp"
-
-    }
-    break_loop_game:
+    #include "2_0_main_loop.cpp"
 
     return 0;
 }
