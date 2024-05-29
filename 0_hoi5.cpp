a245d6342d2a90055fe2a7f6731f18b1b2496754
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 20:46:23 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -8,12 +8,12 @@
 // make the bordering lands BOLD and the other ones ITALIC, and only show factory count if ITALIC, if BOLD show average unit army strength; would be super cool if we could do it on country-per-country basis OR reflect the actual offensive/deffensive strength during war
 //
 // implement visibility, ie only see you country and up to N border of border tiles; also make the rendering and clicking based not on the actual map, but on the tile data in the country struct (this will actually be quite painfull without adding a X and Y properties of the tile struct); also make the rendering the very last thing in the main loop again (if we want to sychronise the clicks with the map rather than the data in the country struct)
+//
+// improve rendering performance by using buffering of some sort
 
 // mouse event tracking: https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Any-event-tracking
 // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797?permalink_comment_id=3878578
 // only get mouse clicks with: code 9 (`h` to turn on; `l` to turn off) (printf "\e[?9h"); I notices that this works in bash and not in fish
-
-// would be cool if we could use a framebuffer of some sort, to shooth out the refreshing
 
 #include "1_0_includes_structs_and_defines.cpp"
 
