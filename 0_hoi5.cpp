b13240ed54146a35b3d9b73f1e49ad9066d48f2b
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 20:40:32 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -4,6 +4,10 @@
 // the fucking vectors can realloc, so I need to check what happens if I use push_back in a function that got passed a pointer/reference to the vector (nothing bad should happen, but I need to make sure)
 //
 // make it so that if the defender just sits there there is some kind of bonus, for example: if the defender has more equipment than you they get a deffensive bonus?
+//
+// make the bordering lands BOLD and the other ones ITALIC, and only show factory count if ITALIC, if BOLD show average unit army strength
+//
+// implement visibility, ie only see you country and up to N border of border tiles; also make the rendering and clicking based not on the actual map, but on the tile data in the country struct (this will actually be quite painfull without adding a X and Y properties of the tile struct); also make the rendering the very last thing in the main loop again (if we want to sychronise the clicks with the map rather than the data in the country struct)
 
 // mouse event tracking: https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Any-event-tracking
 // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797?permalink_comment_id=3878578
