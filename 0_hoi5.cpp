55917b55bedd75da839578b6268e76cb1641b2df
kuche1 <znamsiparolata4esno@abv.bg>
2024-05-29 21:38:54 +0300

    update

--- 0_hoi5.cpp
+++ 0_hoi5.cpp
@@ -17,7 +17,7 @@
 // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797?permalink_comment_id=3878578
 // only get mouse clicks with: code 9 (`h` to turn on; `l` to turn off) (printf "\e[?9h"); I notices that this works in bash and not in fish
 
-#include "1_0_includes_structs_and_defines.cpp"
+#include "1_0_includes_structs_defines.cpp"
 
 int main() {
 
